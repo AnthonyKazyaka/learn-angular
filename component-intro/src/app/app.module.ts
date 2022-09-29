79994d836884f00ee8a04b8d468782c38f1cab15
Anthony Kazyaka <anthonykazyaka@rocketmortgage.com>
2022-09-29 18:12:00 -0400

    Setup ServerComponent to render in the main app

--- component-intro/src/app/app.module.ts
+++ component-intro/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
@@ -11,7 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- component-intro/src/app/server/server.component.ts
+++ component-intro/src/app/server/server.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  //styleUrls: ['./server.component.css']
 })
 export class ServerComponent {
 }
